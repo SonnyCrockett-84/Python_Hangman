e77ed4c1a00add10a8b3b59628d3bb4eb0ad6b72
Marius Morf <marius.morf@ict-campus.net>
2025-10-17 14:03:24 +0200

    Check if letter matches + letter not in word + letter already guessed

--- hangman_advanced.py
+++ hangman_advanced.py
@@ -68,18 +68,29 @@ word = secretWords[random.randint(0, numWords - 1)]
 blanks = []
 
 # List for already guessed letters
-guessedLetters = []
+guessedLetters = {0}
+
+# Boolean to check if letter was already guessed
+alreadyGuessed = False
+
+# Boolean to check if letter is in word
+correctLetter = False
 
 # Create blanks to show the player and show them
 for i in range(len(word)):
     blanks.append("_")
 
-for i in range(len(blanks)):
-    print(blanks[i], end=" ")
+# Number of errors
+errors = 0
 
 print()
 
-for i in range(2):
+while(errors < 6):
+    alreadyGuessed = False
+    correctLetter = False
+
+    print(HANGMANPICS[errors])
+
     print(word)
     
     for i in range(len(blanks)):
@@ -88,6 +99,17 @@ for i in range(2):
     letter = input("Enter a letter > ")
     letter = letter.upper()
 
-    for c in range(len(word)):
-        if word[c] == letter:
+    for d in guessedLetters:
+        if(letter == d):
+            print("This letter has already been guessed!")
+            alreadyGuessed = True
+
+    if not(alreadyGuessed):
+        guessedLetters.add(letter)
+        for c in range(len(word)):
+          if word[c] == letter:
             blanks[c] = letter
+            correctLetter = True
+
+        if not(correctLetter):
+          errors += 1
