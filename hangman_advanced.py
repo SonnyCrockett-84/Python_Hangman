e62e5df7d9ed114afa23a5ada6c65ca89cea8c52
Marius Morf <marius.morf@ict-campus.net>
2025-10-17 15:54:03 +0200

    Added GUI module + basic structre; NOT WORKING, must be changed!

--- hangman_advanced.py
+++ hangman_advanced.py
@@ -1,5 +1,7 @@
 import random
 import os
+from tkinter import *
+from tkinter import messagebox
 
 HANGMANPICS = ['''
 
@@ -59,6 +61,54 @@ HANGMANPICS = ['''
       |
 =========''']
 
+
+###
+#
+# GUI setup
+#
+###
+
+
+root = Tk()
+title = Label(root, text="Hangman Game")
+title.pack()
+root.geometry("400x600")
+
+# Main function
+def main():
+    letter = letter_var.get()
+    letter = letter.upper()
+    alreadyGuessed = alreadyGuessed_var.get()
+    correctLetter = correctLetter_var.get()
+    errors = errors_var.get()
+    correctGuess = correctGuess_var.get()
+  
+   # Only continue if the user type a single letter
+    if(len(letter) > 1 or not letter.isalpha()):
+      messagebox.showinfo("Incorrect input", "Please type in only a single letter (a - z)!")
+
+    else:
+
+      # Check if letter has been guessed already
+      for d in guessedLetters:
+        if(letter == d):
+          messagebox.showinfo("Letter already guessed", "This letter has already been guessed!")
+          alreadyGuessed = True
+
+      if not(alreadyGuessed):
+        guessedLetters.add(letter)
+        for c in range(len(word)):
+          if word[c] == letter:
+            blanks[c] = letter
+            correctGuess += 1
+            correctLetter = True
+
+      if not(correctLetter):
+        errors += 1
+
+      if(correctGuess == len(word)):
+        win = True
+
 # List of words to choose from
 secretWords = ["WATER", "GUITAR", "CAT", "SCHOOL", "MOTORCYCLE", "COMPUTER", "CARAMEL", "COWS", "HUMOR", "CAMPBOARD"]
 numWords = len(secretWords)
@@ -69,10 +119,6 @@ clear = lambda: os.system("cls")
 play = 'y'
 
 while(play == 'y'):
-
-  # Clear console
-  clear()
-
   # Select a random word
   word = secretWords[random.randint(0, numWords - 1)]
 
@@ -83,7 +129,7 @@ while(play == 'y'):
   guessedLetters = {0}
 
   # Number of correctly guessed letter
-  correctGuess = 0
+  correctGuess_var = IntVar(root, value=0)
 
   # Bool to check if player won
   win = False
@@ -93,15 +139,20 @@ while(play == 'y'):
       blanks.append("_")
 
   # Number of errors
-  errors = 0
+  errors_var = IntVar(root, value=0)
 
   # Player has 5 tries to find the correct word
-  while(errors < 6 and not win):
-      alreadyGuessed = False        # Bool to check if letter has been guessed already
-      correctLetter = False         # Bool to check if letter is in word
+  while(errors_var.get() < 6 and not win):
+      alreadyGuessed_var = BooleanVar(root, value=False)        # Bool to check if letter has been guessed already
+      correctLetter_var = BooleanVar(root, value=False)         # Bool to check if letter is in word
 
       # print the current hangman pic
-      print(HANGMANPICS[errors])
+      hangman_label =  Label(root,
+                             text=HANGMANPICS[errors_var.get()],
+                             anchor=CENTER,
+                             justify=CENTER)
+      
+      hangman_label.pack(pady=20)
 
       # Used for debugging
       # print(word)
@@ -110,36 +161,29 @@ while(play == 'y'):
       for i in range(len(blanks)):
         print(blanks[i], end=" ")
 
+      blanks_label = Label(root,
+                           text = blanks,
+                           anchor=CENTER,
+                           justify=CENTER)
+      
+      blanks_label.pack()
+
+      letter_var = StringVar()
+
       # Get letter from user
-      letter = input("Enter a letter > ")
-      letter = letter.upper()
+      letter_entry = Entry(root,
+                     text="Enter a letter > ",
+                     justify=CENTER,
+                     textvariable=letter_var)
+      
+      letter_entry.pack(pady=50)
 
-      # Only continue if the user type a single letter
-      if(len(letter) > 1 or not letter.isalpha()):
-        print()
-        print("Please type in only a single letter (a - z)!")
+      check_button = Button(text="Check letter", command = main)
+      check_button.pack()
 
-      else:
+      root.mainloop()
 
-        # Check if letter has been guessed already
-        for d in guessedLetters:
-            if(letter == d):
-              print("This letter has already been guessed!")
-              alreadyGuessed = True
-
-        if not(alreadyGuessed):
-            guessedLetters.add(letter)
-            for c in range(len(word)):
-              if word[c] == letter:
-                blanks[c] = letter
-                correctGuess += 1
-                correctLetter = True
-
-            if not(correctLetter):
-              errors += 1
-
-        if(correctGuess == len(word)):
-          win = True
+      
 
 
   # Print winning screen
