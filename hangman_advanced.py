40c2d67dda59487f92c41f7ac90efc977d7a93dd
Marius Morf <marius.morf@ict-campus.net>
2025-10-29 15:51:32 +0100

    Edited comments

--- hangman_advanced.py
+++ hangman_advanced.py
@@ -18,10 +18,10 @@ while(play == 'y'):
   clear()
 
   # Select a random word from my list
-  # word = randomWord()
+  word = randomWord()
 
-  # Select a random word from the dictionary
-  word = randomDictionaryWord()
+  # Select a random word from the english dictionary
+  # word = randomDictionaryWord()
 
   # Array for blanks
   blanks = []
@@ -32,33 +32,28 @@ while(play == 'y'):
   # Number of correctly guessed letter
   correctGuess = 0
 
-  # Bool to check if player won
   win = False
 
   # Create blanks for the secret word
   for i in range(len(word)):
       blanks.append("_")
 
-  # Number of errors
   errors = 0
 
   # Player has 5 tries to find the correct word
   while(errors < 6 and not win):
-      alreadyGuessed = False        # Bool to check if letter has been guessed already
-      correctLetter = False         # Bool to check if letter is in word
+      alreadyGuessed = False
+      correctLetter = False
 
-      # print the current hangman pic
       print(HANGMANPICS[errors])
       
-      # Show the blanks and the letters that the player already found
       printBlanks(blanks)
 
-      # Get letter from user
       letter = getLetter()
 
+      # Check if input is 1 character and a letter
       if(checkInput(letter)):
 
-        # Check if letter has been guessed already
         alreadyGuessed = letterAlreadyGuessed(guessedLetters, letter)
 
         if not(alreadyGuessed):
